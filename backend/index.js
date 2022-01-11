81c54179e1311dce526d51c88b0c09e64b8b80c9
Devam Agrawal <dev.devam.a@gmail.com>
2022-01-12 02:08:53 +0530

    Env file configured

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,15 @@
 import cors from "cors";
 import express from "express";
+import color from "colors";
+import dotenv from "dotenv";
 import connecteDB from "./config/db.js";
 import user from "./routes/user.js";
 
 const app = express();
 
 app.use(express.json());
+
+dotenv.config();
 
 connecteDB();
 app.use(cors({ origin: "*", credentials: true }));
