a7229182e9a5f0700831533827de791cd0f65340
Devam Agrawal <dev.devam.a@gmail.com>
2022-01-12 02:09:58 +0530

    database configuration file

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,15 +1,15 @@
 import mongoose from "mongoose";
 const connectDB = async () => {
-  try {
-    const conn = await mongoose.connect(process.env.MONGO_URI, {
-      useUnifiedTopology: true,
-      useNewUrlParser: true,
-    });
+	try {
+		const conn = await mongoose.connect(process.env.MONGO_URI, {
+			useUnifiedTopology: true,
+			useNewUrlParser: true,
+		});
 
-    console.log(`MongoDB Connected: ${conn.connection.host}`.cyan.underline);
-  } catch (error) {
-    console.error(`Error: ${error.message}`.red.underline.bold);
-    process.exit(1);
-  }
+		console.log(`MongoDB Connected: ${conn.connection.host}`.cyan.underline);
+	} catch (error) {
+		console.error(`Error: ${error.message}`.red.underline.bold);
+		process.exit(1);
+	}
 };
 export default connectDB;
