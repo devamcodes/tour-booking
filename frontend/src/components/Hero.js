684d4a170b3f82189722423e6c1b4a599b61ede9
Devam Agrawal <dev.devam.a@gmail.com>
2022-01-12 02:20:26 +0530

    - Hero section component

--- frontend/src/components/Hero.js
+++ frontend/src/components/Hero.js
@@ -1,30 +1,41 @@
 import React from "react";
-import "../App.css";
+// import "../App.css";
 import "./Hero.css";
 import { Buttons } from "./Buttons";
-
+import { FaPlayCircle } from "react-icons/fa";
+import { Parallax } from "react-parallax";
 function Hero() {
+  const image1 =
+    "https://img.traveltriangle.com/blog/wp-content/tr:w-700,h-400/uploads/2017/08/Parvati-Valley.jpg";
   return (
-    <div className="hero-container">
-      {/* <video sc="/assets/video-2.mp4" autoPlay loop muted />s */}
-      <h1>ADVENTURE AWAITS</h1>
-      <p>What are you waiting for?</p>
-      <Buttons
-        className="btns"
-        buttonStyle="btn--outline"
-        buttonSize="btn--large"
+    <>
+      <Parallax
+        bgImage={image1}
+        strength={500}
+        blur={{ min: -4, max: 5 }}
+        className="hero-container"
       >
-        GET STARTED !!!
-      </Buttons>
-      <Buttons
-        className="btns"
-        buttonStyle="btn--primary"
-        buttonSize="btn--large"
-      >
-        WATCH TRAILER
-        <i className="far fa-play-circle" />
-      </Buttons>
-    </div>
+        <h1 className="hero-container-heading">ADVENTURE AWAITS</h1>
+        <p className="hero-container-para">What are you waiting for?</p>
+        <Buttons
+          className="btns"
+          buttonStyle="btn--outline"
+          buttonSize="btn--large"
+        >
+          GET STARTED !!!
+        </Buttons>
+        <Buttons
+          className="btns"
+          buttonStyle="btn--primary"
+          buttonSize="btn--large"
+        >
+          WATCH TRAILER
+          <i className="far fa-play-circle">
+            <FaPlayCircle />
+          </i>
+        </Buttons>
+      </Parallax>
+    </>
   );
 }
 
