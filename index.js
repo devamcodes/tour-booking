70241338c3bb85ff58f2a6c3105cf1d81636fc31
Devam Agrawal <dev.devam.a@gmail.com>
2022-03-13 20:14:10 +0530

    test commit

--- index.js
+++ index.js
@@ -2,9 +2,9 @@ import cors from "cors";
 import express from "express";
 import color from "colors";
 import dotenv from "dotenv";
-import path from "path";
 import connecteDB from "./config/db.js";
 import indexRouter from "./routes/index.js";
+
 const app = express();
 
 app.use(express.json());
@@ -19,12 +19,12 @@ app.use("/", indexRouter);
 // header("Access-Control-Allow-Methods: POST, GET, OPTIONS, PUT, DELETE");
 // header("Access-Control-Allow-Headers: Origin, Content-Type, Accept, Authorization, X-Request-With");
 const PORT = process.env.PORT || 5000;
-if (process.env.NODE_ENV == "production") {
-	app.use(express.static(path.join(__dirname, "client", "build")));
-	app.get("*", (req, res) => {
-		res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-	});
-}
+// if (process.env.NODE_ENV == "production") {
+// 	app.use(express.static(path.join(__dirname, "client", "build")));
+// 	app.get("*", (req, res) => {
+// 		res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+// 	});
+// }
 app.listen(PORT, () => {
 	console.log(`server is listining on ${PORT}`);
 });
